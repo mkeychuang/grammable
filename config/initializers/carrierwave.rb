3b64a98c3c9df547e4a9b976495415ceffbb32a6
mkeychuang <mkey.chuang@gmail.com>
2019-11-08 16:43:23 +0000

    Grammable Lesson 19

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-	config.fog_provider = 'fog/aws'													# required
-	config.fog_public = false
-	config.fog_credentials = {
-		provider:				'AWS',												# required
-		aws_access_key_id:		ENV["AKIA6FAHKGIRTJESQ6S5"],						# required
-		aws_secret_access_key:	ENV["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"]		# required
-	}
-	config.fog_directory = ENV["mchuang-grammable-photos"]							# required
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_public = false
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+  }
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
